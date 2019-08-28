6a27ed2d259c5de245fc92a74fbfe1e0ee237bf2
ThanhNam20 <khuatthanhnam@gmail.com>
2019-08-28 21:43:03 +0700

    Adding Responsive

--- views/screen/news.js
+++ views/screen/news.js
@@ -1,46 +1,16 @@
-const newsScreen = 
-`
-<div class="main-container">
+const newsScreen = `
+<div class="container">
    <div class="main-new">
-     <a href="https://bongda24h.vn/bong-da-anh/alexanderarnold-chuan-bi-san-cho-man-so-tai-voi-arsenal-172-228102.html">
       <div class="block">
-       <img class="img-news" src="https://static.bongda24h.vn/medias/standard/2019/8/23/alexanderarnold-chuan-bi-san-cho-man-so-tai-voi-arsenal.jpg" alt="">
-       <a href="https://bongda24h.vn/bong-da-anh/alexanderarnold-chuan-bi-san-cho-man-so-tai-voi-arsenal-172-228102.html"><h5  class="text">Người Liverpool đã chuẩn bị sẵn cho màn so tài với Arsenal ‘cảm tử’</h5></a>
-       <span class="small-text">Zing Vn</span>
-       <span class="smaller-text"> 4 giờ trước</span>
+          <img class="img-news" src="https://static.bongda24h.vn/medias/standard/2019/8/23/alexanderarnold-chuan-bi-san-cho-man-so-tai-voi-arsenal.jpg">
+          <a href="https://bongda24h.vn/bong-da-anh/alexanderarnold-chuan-bi-san-cho-man-so-tai-voi-arsenal-172-228102.html"><h5 class="text">Người Liverpool đã chuẩn bị sẵn cho màn so tài với Arsenal ‘cảm tử’</h5></a>
+          <span class="small-text">Zing Vn</span>
+          <span class="smaller-text"> 4 giờ trước</span>
      </div>
-    </a>
-
-    <a href="https://bongda24h.vn/bong-da-anh/man-city-gap-hoa-pep-guardiola-van-ung-ho-var-172-228100.html">
-      <div class="block">
-       <img class="img-news" src="https://static.bongda24h.vn/medias/standard/2019/8/23/man-city-gap-hoa-pep-guardiola-van-ung-ho-var.jpg "alt="">
-       <a href="https://bongda24h.vn/bong-da-anh/man-city-gap-hoa-pep-guardiola-van-ung-ho-var-172-228100.html"><h5  class="text">Man City gặp họa, Pep Guardiola vẫn ủng hộ VAR</h5></a>
-       <span class="small-text">Bóng đá 24h</span>
-       <span class="smaller-text"> 4 giờ trước</span>
-     </div>
-    </a>
-    
-    <a href="https://bongda24h.vn/bong-da-anh/alexanderarnold-chuan-bi-san-cho-man-so-tai-voi-arsenal-172-228102.html">
-      <div class="block">
-       <img class="img-news" src="https://static.bongda24h.vn/medias/standard/2019/8/23/alexanderarnold-chuan-bi-san-cho-man-so-tai-voi-arsenal.jpg" alt="">
-       <a href="https://bongda24h.vn/bong-da-anh/alexanderarnold-chuan-bi-san-cho-man-so-tai-voi-arsenal-172-228102.html"><h5  class="text">Người Liverpool đã chuẩn bị sẵn cho màn so tài với Arsenal ‘cảm tử’</h5></a>
-       <span class="small-text">Zing Vn</span>
-       <span class="smaller-text"> 4 giờ trước</span>
-     </div>
-    </a>
-
-    <a href="https://bongda24h.vn/bong-da-anh/alexanderarnold-chuan-bi-san-cho-man-so-tai-voi-arsenal-172-228102.html">
-      <div class="block">
-       <img class="img-news" src="https://static.bongda24h.vn/medias/standard/2019/8/23/alexanderarnold-chuan-bi-san-cho-man-so-tai-voi-arsenal.jpg" alt="">
-       <a href="https://bongda24h.vn/bong-da-anh/alexanderarnold-chuan-bi-san-cho-man-so-tai-voi-arsenal-172-228102.html"><h5  class="text">Người Liverpool đã chuẩn bị sẵn cho màn so tài với Arsenal ‘cảm tử’</h5></a>
-       <span class="small-text">Zing Vn</span>
-       <span class="smaller-text"> 4 giờ trước</span>
-     </div>
-    </a>
-
    </div>
-</div>
-`
+
+  </div>
+`;
 
 function onload() {}
 
